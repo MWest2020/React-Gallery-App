33bfa8d4e56d0ff798e72b75bbef84c136534cef
MWest2020 <mwesterweel@hotmail.com>
2021-01-12 21:26:32 +0100

    fixed catch bug

--- src/Components/PhotoContainer.js
+++ src/Components/PhotoContainer.js
@@ -4,27 +4,28 @@ import Photo from './Photo';
 
 export default class PhotoContainer extends Component{
     render(){
-    const results = this.props.photos;
-    let photos;
-
-    if(results.length > 0){
-        photos = results.map( photo => 
-            <Photo  
-                server={photo.server}
-                id={photo.id}
-                secret={photo.secret}
-                title={photo.title}
-                key={photo.id.toString()}
-                /> 
-            );
-    } else {  photos = <NotFound />; }
         
-    return  (
+        const results = this.props.photos;
+        let photos;
 
-        <div className="photo-container">
-            { !this.props.loading   ?  ''   :   <h1>Loading...</h1> }
-            { this.props.loading    ? ''    :   <ul>{photos}</ul>   }
-        </div>
-        );
+        if(results.length > 0){
+            photos = results.map( photo => 
+                <Photo  
+                    server={photo.server}
+                    id={photo.id}
+                    secret={photo.secret}
+                    title={photo.title}
+                    key={photo.id.toString()}
+                    /> 
+                );
+        } else   {  photos = <NotFound />; }
+            
+        return  (
+
+            <div className="photo-container">
+                { !this.props.loading   ?  ''   :   <h1>Loading...</h1> }
+                { this.props.loading    ? ''    :   <ul>{photos}</ul>   }
+            </div>
+            );
     }  
 } 
--- src/Components/RouteHandling.js
+++ src/Components/RouteHandling.js
@@ -42,31 +42,31 @@ export default class RouteHandling extends Component {
           query,
           photos: response.data.photos.photo,
           loading: false
+      })})
+      .catch((error) => {
+        // Error 😨
+        if (error.response) {
+            /*
+             * The request was made and the server responded with a
+             * status code that falls out of the range of 2xx
+             */
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            /*
+             * The request was made but no response was received, `error.request`
+             * is an instance of XMLHttpRequest in the browser and an instance
+             * of http.ClientRequest in Node.js
+             */
+            console.log(error.request);
+        } else {
+            // Something happened in setting up the request and triggered an Error
+            console.log('Error', error.message);
+        }
+        console.log(error); 
       })
-      // .catch((error) => {
-      //   // Error 😨
-      //   if (error.response) {
-      //       /*
-      //        * The request was made and the server responded with a
-      //        * status code that falls out of the range of 2xx
-      //        */
-      //       console.log(error.response.data);
-      //       console.log(error.response.status);
-      //       console.log(error.response.headers);
-      //   } else if (error.request) {
-      //       /*
-      //        * The request was made but no response was received, `error.request`
-      //        * is an instance of XMLHttpRequest in the browser and an instance
-      //        * of http.ClientRequest in Node.js
-      //        */
-      //       console.log(error.request);
-      //   } else {
-      //       // Something happened in setting up the request and triggered an Error
-      //       console.log('Error', error.message);
-      //   }
-      //   console.log(error); 
-      // })
-    })
+    
   }
     
         
